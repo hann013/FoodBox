23c7b0e9b886ae7e74c23ce01082d0d22bbca1b6
jennadeng96@gmail.com <jennadeng96@gmail.com>
2016-01-23 17:17:53 -0500

    With list and input functions

--- app/src/main/java/com/waterloohacks2015/foodbox/ListActivity.java
+++ app/src/main/java/com/waterloohacks2015/foodbox/ListActivity.java
@@ -1,10 +1,14 @@
 package com.waterloohacks2015.foodbox;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,10 +18,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class ListActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private final int REQUEST_CODE = 100;
+    private final String APP_NAME = "FoodBox";
     private Uri photoUri;
 
     @Override
@@ -31,8 +43,7 @@ public class ListActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent launchCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(launchCamera, 100);
+                takePicture();
             }
         });
 
@@ -101,5 +112,51 @@ public class ListActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            // Image captured and saved to fileUri specified in the Intent
+            Toast.makeText(this, "Image saved to:\n" + photoUri, Toast.LENGTH_LONG).show();
+
+            Intent openPictureIntent = new Intent(Intent.ACTION_VIEW);
+            openPictureIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            openPictureIntent.setDataAndType(photoUri, "image/jpeg");
+            startActivity(openPictureIntent);
+        } else if (resultCode == RESULT_CANCELED) {
+            // Do nothing
+        } else {
+            // Image capture failed
+            Toast.makeText(this, "Image capture failed", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void takePicture() {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        photoUri = getOutputFileUri();
+        Log.d("ListActivity", photoUri.toString());
+        //cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+        startActivityForResult(cameraIntent, REQUEST_CODE);
+    }
+
+    private Uri getOutputFileUri() {
+        File photoDir = new File(
+                this.getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_NAME);
+
+        // Create the storage directory if it does not exist
+        if (!photoDir.exists()){
+            if (!photoDir.mkdirs()){
+                Toast.makeText(this, "Failed to get photo directory", Toast.LENGTH_LONG).show();
+                return null;
+            }
+        }
+
+        // Create a photo file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.CANADA).format(new Date());
+        File photoFile = new File(photoDir.getPath() + File.separator + "IMG_"+ timeStamp + ".jpg");
+        return Uri.fromFile(photoFile);
     }
 }
--- app/src/main/java/com/waterloohacks2015/foodbox/MainActivity.java
+++ app/src/main/java/com/waterloohacks2015/foodbox/MainActivity.java
@@ -1,18 +1,35 @@
 package com.waterloohacks2015.foodbox;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import db.ListDBHelper;
+
+public class MainActivity extends Activity {
+
+    ListDBHelper db;
+    SimpleCursorAdapter listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        updateUI();
+
+/*
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -23,7 +40,101 @@ public class MainActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
+    }
+    @Override
+    public void onResume() {
+        super.onResume();  // Always call the superclass method first
+        updateUI();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        Log.d("Main Activity", "Menu Inflate");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add_food:
+                Intent i = new Intent(getApplicationContext(), NewFood.class);
+                this.startActivity(i);
+                return true;
+
+            case R.id.send_notification:
+                Intent j = new Intent(getApplicationContext(), NotificationActivity.class);
+                this.startActivity(j);
+                return true;
+
+            default:
+                return false;
+        }
+    }
+
+
+    private void updateUI() {
+        db = new ListDBHelper(this);
+        Cursor cursor = db.displayData();
+
+        String[] columns = new String[]{
+                db.COL_2,
+                db.COL_3,
+                db.KEY_ID
+        };
+
+        int[] to = new int[] {
+                R.id.foodName,
+                R.id.expirationDate,
+        };
+
+        MyCursorAdapter dataAdapter = new MyCursorAdapter(this, R.layout.food_view, cursor,
+            columns, to, 0 );
+
+    ListView listView = (ListView) findViewById(R.id.list);
+    // Assign adapter to ListView
+    listView.setAdapter(dataAdapter);
+
+    }
+
+    private class MyCursorAdapter extends SimpleCursorAdapter {
+
+        public MyCursorAdapter(Context context, int layout, Cursor c,
+                               String[] from, int[] to, int flags) {
+            super(context, layout, c, from, to, flags);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            //get reference to the row
+            View view = super.getView(position, convertView, parent);
+            //check for odd or even to set alternate colors to the row background
+            if (position % 2 == 0) {
+                view.setBackgroundColor(Color.rgb(238, 233, 233));
+            } else {
+                view.setBackgroundColor(Color.rgb(255, 255, 255));
+            }
+            return view;
+        }
+    }
+
+    public void onEatenButtonClick(View view) {
+        View v = (View) view.getParent();
+        TextView foodItemV = (TextView) v.findViewById(R.id.foodName);
+        String food = foodItemV.getText().toString();
+
+        String sql = String.format("DELETE FROM %s WHERE %s = '%s'",
+                db.TABLE_NAME,
+                db.COL_2,
+                food);
+
+
+        db = new ListDBHelper(MainActivity.this);
+        SQLiteDatabase sqlDB = db.getWritableDatabase();
+        sqlDB.execSQL(sql);
+        updateUI();
+    }
 }
+
